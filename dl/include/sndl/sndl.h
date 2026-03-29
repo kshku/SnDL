079fe72c900645b67904a7185400a6be983b9f28
kshku <1211shree@gmail.com>
2026-03-29 20:09:12 +0530

    separate api def

--- dl/include/sndl/sndl.h
+++ dl/include/sndl/sndl.h
@@ -2,27 +2,7 @@
 
 #include "sndl/defines.h"
 
-#if defined(SN_DL_STATIC)
-    #define SN_API
-#else
-    #ifdef SN_EXPORT
-        #if defined(SN_OS_LINUX) || defined(SN_OS_MAC)
-            #define SN_API __attribute__((visibility("default")))
-        #elif defined(SN_OS_WINDOWS)
-            #define SN_API __declspec(dllexport)
-        #else
-            #error "Should not reach here!"
-        #endif
-    #else
-        #if defined(SN_OS_LINUX) || defined(SN_OS_MAC)
-            #define SN_API
-        #elif defined(SN_OS_WINDOWS)
-            #define SN_API __declspec(dllimport)
-        #else
-            #error "Should not reach here!"
-        #endif
-    #endif
-#endif
+#include "sndl/api.h"
 
 typedef enum snDLOpenFlags {
     SN_DL_OPEN_FLAG_NONE = 0,

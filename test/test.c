c6810f467a8136f8f70632d78137d1a99718ec8b
kshku <1211shree@gmail.com>
2026-06-07 16:24:00 +0530

    Replace assert() with TEST_ASSERT in tests, guard SN_ASSERT behind SN_DEBUG

--- test/test.c
+++ test/test.c
@@ -1,54 +1,56 @@
-#include <assert.h>
 #include <sndl/sndl.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define TEST_ASSERT(x) do { if (!(x)) { fprintf(stderr, "FAIL [%s:%d]: %s\n", __FILE__, __LINE__, #x); abort(); } } while(0)
 
 typedef int (*dl_binop_fn)(int, int);
 
 static void test_basic_load(void) {
     void *handle = sn_dl_open(SN_DL_TEST_LIB, SN_DL_OPEN_FLAG_DEFAULT);
-    assert(handle && "sn_dl_open failed");
+    TEST_ASSERT(handle && "sn_dl_open failed");
 
     dl_binop_fn add = (dl_binop_fn)sn_dl_get_symbol(handle, "dl_test_add");
-    assert(add && "sn_dl_get_symbol(add) failed");
+    TEST_ASSERT(add && "sn_dl_get_symbol(add) failed");
 
     dl_binop_fn sub = (dl_binop_fn)sn_dl_get_symbol(handle, "dl_test_sub");
-    assert(sub && "sn_dl_get_symbol(sub) failed");
+    TEST_ASSERT(sub && "sn_dl_get_symbol(sub) failed");
 
-    assert(add(2, 3) == 5);
-    assert(sub(7, 4) == 3);
+    TEST_ASSERT(add(2, 3) == 5);
+    TEST_ASSERT(sub(7, 4) == 3);
 
-    assert(sn_dl_close(handle));
+    TEST_ASSERT(sn_dl_close(handle));
 }
 
 static void test_flags(void) {
     void *handle = sn_dl_open(SN_DL_TEST_LIB, SN_DL_OPEN_FLAG_NOW | SN_DL_OPEN_FLAG_LOCAL);
 
-    assert(handle && "sn_dl_open_ex failed");
+    TEST_ASSERT(handle && "sn_dl_open_ex failed");
 
     dl_binop_fn add = (dl_binop_fn)sn_dl_get_symbol(handle, "dl_test_add");
 
-    assert(add);
-    assert(add(10, 5) == 15);
+    TEST_ASSERT(add);
+    TEST_ASSERT(add(10, 5) == 15);
 
-    assert(sn_dl_close(handle));
+    TEST_ASSERT(sn_dl_close(handle));
 }
 
 static void test_invalid_symbol(void) {
     void *handle = sn_dl_open(SN_DL_TEST_LIB, SN_DL_OPEN_FLAG_NONE);
-    assert(handle);
+    TEST_ASSERT(handle);
 
     void *sym = sn_dl_get_symbol(handle, "this_symbol_does_not_exist");
-    assert(sym == NULL);
+    TEST_ASSERT(sym == NULL);
 
-    assert(sn_dl_close(handle));
+    TEST_ASSERT(sn_dl_close(handle));
 }
 
 static void test_double_close(void) {
     void *handle = sn_dl_open(SN_DL_TEST_LIB, SN_DL_OPEN_FLAG_NONE);
-    assert(handle);
+    TEST_ASSERT(handle);
 
-    assert(sn_dl_close(handle));
-    assert(!sn_dl_close(handle)); /* must fail safely */
+    TEST_ASSERT(sn_dl_close(handle));
+    TEST_ASSERT(!sn_dl_close(handle)); /* must fail safely */
 }
 
 int main(void) {

35f165eaa19c7b616bd6372b6b983f3f0d5a3293
kshku <1211shree@gmail.com>
2026-06-11 10:07:03 +0530

    style: apply clang-format to source

--- dl/src/win32/sndl.c
+++ dl/src/win32/sndl.c
@@ -2,14 +2,13 @@
 
 #if defined(SN_OS_WINDOWS)
 
-    #include <windows.h>
     #include <sncore/utf.h>
+    #include <windows.h>
 
 void *sn_dl_open(const char *path, int32_t flags) {
     SN_UNUSED(flags);
     wchar_t wpath[4096];
-    if (sn_utf8_to_utf16(path, wpath, SN_ARRAY_LENGTH(wpath)) == (size_t)-1)
-        return NULL;
+    if (sn_utf8_to_utf16(path, wpath, SN_ARRAY_LENGTH(wpath)) == (size_t)-1) return NULL;
     return (void *)LoadLibraryW(wpath);
 }
 

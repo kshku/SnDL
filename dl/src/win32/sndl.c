c535c2d272dd607cacffc64ecb95db25abfd530c
kshku <1211shree@gmail.com>
2026-06-11 07:48:01 +0530

    feat: switch Windows backend from LoadLibraryA to LoadLibraryW
    - Convert UTF-8 path to UTF-16 via sn_utf8_to_utf16()
    - Call LoadLibraryW instead of LoadLibraryA

--- dl/src/win32/sndl.c
+++ dl/src/win32/sndl.c
@@ -3,10 +3,14 @@
 #if defined(SN_OS_WINDOWS)
 
     #include <windows.h>
+    #include <sncore/utf.h>
 
 void *sn_dl_open(const char *path, int32_t flags) {
     SN_UNUSED(flags);
-    return (void *)LoadLibraryA(path);
+    wchar_t wpath[4096];
+    if (sn_utf8_to_utf16(path, wpath, SN_ARRAY_LENGTH(wpath)) == (size_t)-1)
+        return NULL;
+    return (void *)LoadLibraryW(wpath);
 }
 
 void *sn_dl_get_symbol(void *handle, const char *symbol) {
